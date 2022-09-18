172b8d7c74619123109d9fc0cfd2278b16c009d1
leonardosantos-ifood <leonardo.santos@ifood.com.br>
2022-09-18 03:50:29 -0300

    Add unit tests for MatchesResponseToMatchListAdapter

--- CSTVTests/Modules/MatchList/Domain/MatchesResponseToMatchListAdapterTests.swift
+++ CSTVTests/Modules/MatchList/Domain/MatchesResponseToMatchListAdapterTests.swift
@@ -1,8 +1,25 @@
-//
-//  MatchesResponseToMatchListAdapter.swift
-//  CSTVTests
-//
-//  Created by Leonardo Coutinho Santos on 18/09/22.
-//
+import XCTest
 
-import Foundation
+@testable import CSTV
+
+final class MatchesResponseToMmatchListAdapterTests: XCTestCase {
+    private let sut = MatchList(
+        from: .fixture(
+            id: 0,
+            games: [.fixture(status: .finished)],
+            serie: .fixture(name: "serie name"),
+            league: .fixture(name: "league name", imageUrl: "league image"),
+            opponents: [.fixture(), .fixture()]
+        )
+    )
+    
+    func test_shouldAdaptPropertiesCorrectly() {
+        XCTAssertEqual(sut.id, 0)
+        XCTAssertEqual(sut.status, .finished)
+        XCTAssertEqual(sut.leagueName, "league name")
+        XCTAssertEqual(sut.leagueImageUrl, "league image")
+        XCTAssertEqual(sut.serieName, "serie name")
+        
+        XCTAssertEqual(sut.opponents.count, 2)
+    }
+}
